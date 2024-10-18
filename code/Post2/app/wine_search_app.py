d31c381da04b7923a9eb4a5b62f462ae4c448171
Anne-Lise Saive <97337223+annelisesaive@users.noreply.github.com>
2024-10-18 16:14:01 +0100

    Update wine_search_app.py

--- code/Post2/app/wine_search_app.py
+++ code/Post2/app/wine_search_app.py
@@ -5,7 +5,6 @@ import streamlit as st
 interesting_wines_path = './data/concise_interesting_wines.csv'
 wine_data = pd.read_csv(interesting_wines_path)
 
-
 # Title of the app
 st.markdown("""
             # Welcome to QUALITYSIP 🍷
@@ -13,7 +12,7 @@ st.markdown("""
             """)
 
 # Add the logo of the app
-image_path = 'QualitySip_logo.png'  
+image_path = './QualitySip_logo.png'  # Ensure the image is in the same directory
 st.image(image_path, caption=None, use_column_width=True)
 
 # App explanation & functionalities
